7066a377d9ef8bd02e46da742c795dc5940b08c6
ioakeimdi <135948980+ioakeimdi@users.noreply.github.com>
2023-06-12 17:16:29 +0300

    Create wc_check_user_blog_membership.php

---
+++ wc_check_user_blog_membership.php
@@ -0,0 +1,17 @@
+// Check if a user from multisite belongs to this site to login
+
+add_filter('woocommerce_process_login_errors', 'wc_check_user_blog_membership', 20, 3);
+function wc_check_user_blog_membership($validation_error, $username, $password) {
+	
+	if(is_email($username)){
+		$user = get_user_by('email', $username);
+	}else{
+		$user = get_user_by('login', $username);
+	}
+	
+	if($user && !is_user_member_of_blog($user->ID, get_current_blog_id())){
+		$validation_error->add('authentication_failed', __('Δεν είστε μέλος αυτού του ιστοτόπου', 'woocommerce'));
+	}
+
+	return $validation_error;
+}
