8e2bfa6710707fee8d77350014f6d256fefcb778
id <id@timeless.gr>
2023-09-25 17:01:58 +0300

    commit

--- wc-multisite-check-user-blog-membership.php
+++ wc-multisite-check-user-blog-membership.php
@@ -3,7 +3,7 @@
 
 add_filter('woocommerce_process_login_errors', 'wc_check_user_blog_membership', 20, 3);
 function wc_check_user_blog_membership($validation_error, $username, $password) {
-	
+
 	if(is_email($username)){
 		$user = get_user_by('email', $username);
 	}else{
